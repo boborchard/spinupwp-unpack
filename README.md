1edea923794c296a6722290be698e46c04f99503
boborchard <bob@webnomads.io>
2019-12-06 20:45:50 -0600

    Add migrate script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# spinupwp-unpack
+# SpinupWP Unpack Script
+
 Unpack a WordPress backup and load into a SpinupWP Site
+
+> This script assumes that you are uploading a zip file that contains just the /wp-content folder and a .sql file for the database. It does not care about the name of the files, just that they exist.
+
+To use this script:
+
+1. Upload your .zip backup as well as this script to `/` (above `/files`) for the site you're going to unpack.
+2. SSH into your site and run `sh unpack.sh`
+3. Follow the given prompts - the script will clean itself up when it completes.
+4. Clear the page cache in SpinupWP's dashboard for the site
+
+That's it!
---
+++ unpack.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# Unpack the payload
+mkdir 'unpack'
+unzip '*.zip' -d backup
+
+# Move down to the files directory for wp-cli
+cd files
+
+# Grab the credentials out of wp-config.php
+WPDBNAME=`cat wp-config.php | grep DB_NAME | cut -d \' -f 4`
+WPDBUSER=`cat wp-config.php | grep DB_USER | cut -d \' -f 4`
+WPDBPASS=`cat wp-config.php | grep DB_PASSWORD | cut -d \' -f 4`
+
+# Import the Database
+wp db clean
+wp db import ../unpack/backup.sql
+
+# Update old URLs
+read -p 'Original Domain (with http): ' originaldomain
+read -p 'New Domain (with http): ' newdomain
+wp search-replace $originaldomain $newdomain
+wp cache flush
+
+# Move up to root
+cd ~
+
+# Move everything to /wp-content
+rsync -avh --progress unpack/files/wp-content/ files/wp-content
+
+# Clean up
+rm -rf ~/unpack
+rm -rf ~/migrate.sh
+rm -rf ~/*.sql
+rm -rf ~/*.zip
